c35f4869e70e6bfb90850a142033b8660039762b
rodrigopastorfelis <84048157+rodrigopastorfelis@users.noreply.github.com>
2023-02-09 12:31:11 -0300

    t3

--- app/Models/simulacao.php
+++ app/Models/simulacao.php
@@ -21,47 +21,26 @@ class simulacao extends Model
       $dividendo=0;
       if($pesonotas->matematica !=0){
         $soma += $this->matematica * $pesonotas->matematica;
-        $dividendo +=2;
-      }
-      else{
-        $soma += $this->matematica;
-        $dividendo ++;
       }
       if($pesonotas->humanas !=0){
         $soma += $this->humanas * $pesonotas->humanas;
-        $dividendo +=2;
-      }
-      else{
-        $soma += $this->humanas;
-        $dividendo +=2;
       }
       if($pesonotas->redacao !=0){
         $soma += $this->redacao * $pesonotas->redacao;
-        $dividendo +=2;
+        
       }
-      else{
-        $soma += $this->redacao;
-        $dividendo ++;
-      }
+      
       if($pesonotas->linguagens !=0){
         $soma += $this->linguagens * $pesonotas->linguagens;
         $dividendo +=2;
-      }
-      else{
-        $soma += $this->linguagens;
-        $dividendo ++;
       }
       if($pesonotas->natureza !=0){
         $soma += $this->natureza * $pesonotas->natureza;
         $dividendo +=2;
       }
-      else{
-        $soma += $this->natureza;
-        $dividendo ++;
-      }
 
       // $soma= ($this->matematica * $pesonotas->matematica) + ($this->humanas * $pesonotas->humanas) + ($this->redacao * $pesonotas->redacao) + ($this->linguagens * $pesonotas->linguagens) + ($this->natureza * $pesonotas->natureza);
-      $total = $soma /$dividendo ;
+      $total = $soma /10 ;
       return $total;
     }
 }
