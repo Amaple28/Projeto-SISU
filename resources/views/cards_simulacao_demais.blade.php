c996372d67cba780bb04b08536905161bb942e0d
rodrigopastorfelis <rodrigopastorfelis@gmail.com>
2023-02-09 17:19:36 -0300

    v

--- resources/views/cards_simulacao_demais.blade.php
+++ resources/views/cards_simulacao_demais.blade.php
@@ -28,10 +28,17 @@
                         <div class="col-12">
                             <p class="text-muted chances">
                                 @if(!$faculdade->getCalculoAnterior($user->id, $estado))
+                                @dump('Gus')
                                 <i class="fas fa-long-arrow-alt-down"></i>
-                                Nota final abaixo da nota de corte. <b>Sua nota:{{$faculdade->getCalculaNotaUserFacul($user->id,$estado)}}</b>
+                                Nota final abaixo da nota de corte.<b>Sua nota: {{$faculdade->getCalculoAnterior($user->id,$estado)}}</b>
                                 <i class="fas fa-frown"></i>
+                                @elseif ($faculdade->getCalculoAnterior($user->id, $estado) === 'zero')
+                                @dump('Ta')
+                                <i class="fa-duotone fa-circle-xmark"></i>
+                                Nota de Corte Zerada
+                                <i class="fa-solid fa-face-grin-beam-sweat"></i>
                                 @else
+                                @dump('Vo')
                                 <i class="fas fa-long-arrow-alt-up"></i>
                                 Nota final acima da nota de corte.
                                 <i class="fas fa-laugh-beam"></i>
