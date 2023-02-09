6e8c34c3155d4f301ef34b5caaa938a551778547
rodrigopastorfelis <84048157+rodrigopastorfelis@users.noreply.github.com>
2023-02-09 12:31:54 -0300

    t4

--- app/Models/simulacao.php
+++ app/Models/simulacao.php
@@ -19,25 +19,22 @@ class simulacao extends Model
       $pesonotas = PesoNotas::where('faculdade_id', $id)->first();
       $soma = 0;
       $dividendo=0;
-      if($pesonotas->matematica !=0){
+     
         $soma += $this->matematica * $pesonotas->matematica;
-      }
-      if($pesonotas->humanas !=0){
+
         $soma += $this->humanas * $pesonotas->humanas;
-      }
-      if($pesonotas->redacao !=0){
+      
         $soma += $this->redacao * $pesonotas->redacao;
         
-      }
       
-      if($pesonotas->linguagens !=0){
+      
+      
         $soma += $this->linguagens * $pesonotas->linguagens;
-        $dividendo +=2;
-      }
-      if($pesonotas->natureza !=0){
+        
+  
         $soma += $this->natureza * $pesonotas->natureza;
-        $dividendo +=2;
-      }
+        
+      
 
       // $soma= ($this->matematica * $pesonotas->matematica) + ($this->humanas * $pesonotas->humanas) + ($this->redacao * $pesonotas->redacao) + ($this->linguagens * $pesonotas->linguagens) + ($this->natureza * $pesonotas->natureza);
       $total = $soma /10 ;
