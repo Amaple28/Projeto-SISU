3b4bd1c01b69401a570de1a60a2a56486063a3d7
rodrigopastorfelis <84048157+rodrigopastorfelis@users.noreply.github.com>
2023-02-09 12:17:26 -0300

    dividendo

--- app/Models/simulacao.php
+++ app/Models/simulacao.php
@@ -18,39 +18,50 @@ class simulacao extends Model
     public function pesoNotas($id){
       $pesonotas = PesoNotas::where('faculdade_id', $id)->first();
       $soma = 0;
+      $dividendo=0;
       if($pesonotas->matematica !=0){
         $soma += $this->matematica * $pesonotas->matematica;
+        $dividendo +=2;
       }
       else{
         $soma += $this->matematica;
+        $dividendo ++;
       }
       if($pesonotas->humanas !=0){
         $soma += $this->humanas * $pesonotas->humanas;
+        $dividendo +=2;
       }
       else{
         $soma += $this->humanas;
+        $dividendo +=2;
       }
       if($pesonotas->redacao !=0){
         $soma += $this->redacao * $pesonotas->redacao;
+        $dividendo +=2;
       }
       else{
         $soma += $this->redacao;
+        $dividendo ++;
       }
       if($pesonotas->linguagens !=0){
         $soma += $this->linguagens * $pesonotas->linguagens;
+        $dividendo +=2;
       }
       else{
         $soma += $this->linguagens;
+        $dividendo ++;
       }
       if($pesonotas->natureza !=0){
         $soma += $this->natureza * $pesonotas->natureza;
+        $dividendo +=2;
       }
       else{
         $soma += $this->natureza;
+        $dividendo ++;
       }
 
       // $soma= ($this->matematica * $pesonotas->matematica) + ($this->humanas * $pesonotas->humanas) + ($this->redacao * $pesonotas->redacao) + ($this->linguagens * $pesonotas->linguagens) + ($this->natureza * $pesonotas->natureza);
-      $total = ($soma / 10)/5;
+      $total = $soma /$dividendo;
       return $total;
     }
 }
