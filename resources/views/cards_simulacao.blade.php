e40310fb5aed92ac67e97c129a234e148b48ed66
rodrigopastorfelis <84048157+rodrigopastorfelis@users.noreply.github.com>
2023-02-09 19:22:42 -0300

    a

--- resources/views/cards_simulacao.blade.php
+++ resources/views/cards_simulacao.blade.php
@@ -16,7 +16,7 @@
             </div>
 
             <div class="collapse show" id="collapsecard{{$faculdade_escolhida->id}}">
-                <div class="card @if($faculdade_escolhida->getsisu_anterior()==0) zerada @elseif(!$faculdade_escolhida->getCalculoAnterior($user->id, $estado)) reprovado @elseif ($faculdade_escolhida->getCalculoAnterior($user->id, $estado) == 'zero') zerada  @else aprovado @endif">
+                <div class="card @if($faculdade_escolhida->getsisu_anterior()<=1) zerada @elseif(!$faculdade_escolhida->getCalculoAnterior($user->id, $estado)) reprovado @elseif ($faculdade_escolhida->getCalculoAnterior($user->id, $estado) == 'zero') zerada  @else aprovado @endif">
                     <div class="card-body">
                         <h5 class="card-title mb-3">{{$faculdade_escolhida->estado}} - {{$faculdade_escolhida->nome}} {{$faculdade_escolhida->endereco}} ({{$faculdade_escolhida->modalidade}})</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Nota de Corte 2022: {{$faculdade_escolhida->getsisu_anterior()}} *</h6>
@@ -43,7 +43,7 @@
             </div>
 
             <div class="collapse" id="collapsecard2{{$faculdade_escolhida->id}}">
-                <div class="card    @if(!$faculdade_escolhida->getCalculoAtual($user->id, $estado)) reprovado @elseif($faculdade_escolhida->getCalculoAtual($user->id, $estado)) aprovado  @else zerada @endif">
+                <div class="card @if($faculdade_escolhida->getsisu_atual()<=1) zerada @if(!$faculdade_escolhida->getCalculoAtual($user->id, $estado)) reprovado @elseif ($faculdade_escolhida->getCalculoAtual($user->id, $estado) == 'zero') zerada  @else aprovado @endif">
                     <div class="card-body">
                         <h5 class="card-title mb-3">{{$faculdade_escolhida->estado}} - {{$faculdade_escolhida->nome}} {{$faculdade_escolhida->endereco}} ({{$faculdade_escolhida->modalidade}})</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Nota de Corte 2023: {{$faculdade_escolhida->getsisu_atual()}} *</h6>
@@ -55,7 +55,7 @@
                                     <i class="fas fa-long-arrow-alt-down"></i>
                                     Nota final abaixo da nota de corte.<b>Sua nota: {{$faculdade_escolhida->getCalculaNotaUserFacul($user->id,$estado)}}</b>
                                     <i class="fas fa-frown"></i>
-                                    @elseif($faculdade_escolhida->getCalculoAtual($user->id, $estado))
+                                    @elseif ($faculdade_escolhida->getCalculoAtual($user->id, $estado) === 'zero')
                                     Nota de Corte Zerada
                                     @else
                                     <i class="fas fa-long-arrow-alt-up"></i>
