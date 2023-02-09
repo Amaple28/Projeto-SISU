778fb52eac9a0abb8197f4947fcce1a56afadc36
rodrigopastorfelis <rodrigopastorfelis@gmail.com>
2023-02-09 17:01:23 -0300

    .

--- resources/views/cards_simulacao.blade.php
+++ resources/views/cards_simulacao.blade.php
@@ -20,14 +20,15 @@
                     <div class="card-body">
                         <h5 class="card-title mb-3">{{$faculdade_escolhida->estado}} - {{$faculdade_escolhida->nome}} {{$faculdade_escolhida->endereco}} ({{$faculdade_escolhida->modalidade}})</h5>
                         <h6 class="card-subtitle mb-2 text-muted">Nota de Corte 2022: {{$faculdade_escolhida->getsisu_anterior()}} *</h6>
-                        {{-- <h6 class="card-subtitle mb-2 text-muted">Nota de Corte 2023: {{$faculdade_escolhida->getsisu_atual()}}</h6> --}}
 
                         <div class="quadro_resultado">
                             <div class="col-12">
                                 <p class="text-muted chances">
                                     @if(!$faculdade_escolhida->getCalculoAnterior($user->id, $estado))
                                     <i class="fas fa-long-arrow-alt-down"></i>
-                                    Nota final abaixo da nota de corte. <b>Sua nota: {{$faculdade_escolhida->getCalculaNotaUserFacul($user->id,$estado)}}</b>
+                                    <span>
+                                        Nota final abaixo da nota de corte. <b>Sua nota:    {{$faculdade_escolhida->getCalculaNotaUserFacul($user->id,$estado)}}</b>
+                                    </span>
                                     <i class="fas fa-frown"></i>
                                     @else
                                     <i class="fas fa-long-arrow-alt-up"></i>
@@ -55,7 +56,7 @@
                                     <i class="fas fa-long-arrow-alt-down"></i>
                                     Nota final abaixo da nota de corte.<b>Sua nota: {{$faculdade_escolhida->getCalculaNotaUserFacul($user->id,$estado)}}</b>
                                     <i class="fas fa-frown"></i>
-                                    @else
+                                    @elseif ($faculdade_escolhida->getCalculoAtual($user->id, $estado) === true)
                                     <i class="fas fa-long-arrow-alt-up"></i>
                                     Nota final acima da nota de corte.
                                     <i class="fas fa-laugh-beam"></i>
