982f12001a3e4161eb0d6df76b35289ac93c4056
rodrigopastorfelis <rodrigopastorfelis@gmail.com>
2023-02-10 17:23:22 -0300

    filter for colleges is working

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,18 +1,52 @@
-function adicionarClasse (status) {
+const cards = document.querySelectorAll('.full-card');
+const filtros = document.querySelectorAll('.filtro')
 
-    // let selector;
-
-    // if(status === 'reprovado') selector = 'aprovado';
-
-    // if(status === 'aprovado') selector = 'reprovado'
-
-
-    const cards = document.querySelectorAll('.full-card');
-
+function adicionarClasse(status) {
     cards.forEach(item => {
         const card = item.querySelector('.card');
         if (!card.classList.contains(status)) {
-        item.classList.toggle('d-none');
-      }
+            item.classList.toggle('d-none');
+        }
     });
-  };
+};
+
+function verifyFilterIsActive(element) {
+    if (element.classList.contains('active_filter')) {
+        return true
+    }
+    return false
+}
+
+function clearFilter() {
+    filtros.forEach(item => {
+        item.classList.remove('active_filter');
+    });
+    cards.forEach(item => {
+        item.classList.remove('d-none');
+    });
+}
+
+
+
+function showAprovadas(element) {
+    if (verifyFilterIsActive(element)) {
+        clearFilter();
+        return
+    }
+
+    clearFilter();
+    element.classList.add('active_filter');
+    adicionarClasse('aprovado');
+}
+
+function showReprovadas(element) {
+    if (verifyFilterIsActive(element)) {
+        clearFilter();
+        return
+    }
+    clearFilter();
+    element.classList.toggle('active_filter');
+    adicionarClasse('reprovado');
+}
+
+
